df25288880360fa2654d8a44765f7409106bec2a
stevecyj <ycsteven.tsao@gmail.com>
2023-06-17 07:25:01 +0800

    feat: add date picker feature using v-calendar and popperjs
    - Add `@popperjs/core` and `v-calendar` as dependencies in `package.json`
    - Add a `VDatePicker` component to `pages/index.vue`
    - Create a new plugin `calendar.ts` that imports and uses `v-calendar`
    - No changes in `package-lock.json` and `yarn.lock` files
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
 // const { count, addCount } = useAddCount();
 const { $hello } = useNuxtApp();
 const store = useHomeStore();
+const selectedDate = ref(new Date());
 </script>
 
 <template>
@@ -9,6 +10,7 @@ const store = useHomeStore();
     <h1>index:{{ store.count }}</h1>
     <h2>{{ $hello('IronMan') }}</h2>
     <h2 v-timeformat="1686925898493"></h2>
+    <VDatePicker v-model="selectedDate" />
     <button @click="store.addCount">add</button>
     <Home />
     <Box />
