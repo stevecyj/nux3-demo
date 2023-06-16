458b77cad52adbce6d0a6af05d097faec788e721
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-16 17:20:49 +0800

    feat: refactor index.vue to use new Home.js store
    - Remove the `count` and `addCount` functions from the `index.vue` file
    - Add a new `Home.js` file to the project
    - Define a new store called `home` in the `Home.js` file
    - Add a `count` property and an `addCount` method to the `home` store
    - Change the code in the `index.vue` file to use the `count` and `addCount` properties from the `home` store
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <script setup>
-const { count, addCount } = useAddCount();
+// const { count, addCount } = useAddCount();
+const store = useHomeStore();
 </script>
 
 <template>
   <div>
-    <h1>index:{{ count }}</h1>
-    <button @click="addCount">add</button>
+    <h1>index:{{ store.count }}</h1>
+    <button @click="store.addCount">add</button>
     <Home />
     <Box />
   </div>
