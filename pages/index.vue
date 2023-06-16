fe4e97a24bd256aa8db5f296336268837706685f
stevecyj <ycsteven.tsao@gmail.com>
2023-06-16 22:29:02 +0800

    feat: add new plugin for greeting functionality
    - Remove the `prettier.disableLanguages` option from `.vscode/settings.json`
    - Add a new file `plugins/hello.ts` with a plugin that provides a `hello` function
    - In `pages/index.vue`, use the `hello` function provided by the `plugins/hello.ts` plugin
    - Update the script section of `pages/index.vue` to use `useNuxtApp()` instead of `useAddCount()`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 // const { count, addCount } = useAddCount();
+const { $hello } = useNuxtApp();
 const store = useHomeStore();
 </script>
 
 <template>
   <div>
     <h1>index:{{ store.count }}</h1>
+    <h2>{{ $hello('IronMan') }}</h2>
     <button @click="store.addCount">add</button>
     <Home />
     <Box />
