bd53c8ee2133cad6c4a1ccd4283cc813ef6de6c4
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-17 17:55:32 +0800

    feat: add new stylesheet and script links to about page metadata
    - Add new stylesheet and script links to the `about.vue` page
    - Use the `useHead` hook to add the new links
    - Update the title and description metadata for the `about.vue` page
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/about.vue
+++ pages/about.vue
@@ -17,6 +17,21 @@ definePageMeta({
 //   ],
 // });
 
+useHead({
+  link: [
+    {
+      rel: "stylesheet",
+      href: "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css",
+    },
+  ],
+  script: [
+    {
+      src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js",
+      async: true,
+    },
+  ],
+});
+
 useSeoMeta({
   title: "About - Nuxt3 高效入門全攻略",
   description: "關於我們 - 最棒的Nuxt3的線上課程",
