e68e316e4d977cb7ac0aac358ec6abda2ba209af
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-17 17:40:39 +0800

    refactor: refactor useHead to useSeoMeta in about and index pages
    - Update the useHead function to useSeoMeta function in both `about.vue` and `index.vue` files.
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,8 @@ const fetchData = async () => {
     <ClientOnly>
       <h2>{{ $hello("IronMan") }}</h2>
     </ClientOnly>
-    <NuxtLink to="/user">user</NuxtLink>
+    <NuxtLink to="/user">user</NuxtLink> |
+    <NuxtLink to="/about">about</NuxtLink>
     <h2 v-timeformat="1686925898493"></h2>
     <VDatePicker v-model="selectedDate" />
     <button @click="store.addCount">add</button>
