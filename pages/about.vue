288bb80ce879d54788c37cb9110d507b27e82aae
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-17 17:31:02 +0800

    feat: add metadata and components for About page
    - Add metadata for page titles and descriptions
    - Add metadata for social media sharing
    - Import components for About page header and footer
    - Enable custom layout for About page
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/about.vue
+++ pages/about.vue
@@ -2,6 +2,21 @@
 definePageMeta({
   layout: "default",
 });
+
+useHead({
+  title: "關於我們 About - Nuxt3 高效入門全攻略",
+  meta: [
+    { property: "og:title", content: "關於我們 About - Nuxt3 高效入門全攻略" },
+    { property: "og:url", content: "http://localhost:3000/about" },
+    { property: "og:image", content: "http://localhost:3000/share.jpg" },
+    { name: "description", content: "關於我們 About - 最棒的Nuxt3的線上課程" },
+    {
+      property: "og:description",
+      content: "關於我們 About - 最棒的Nuxt3的線上課程",
+    },
+  ],
+});
+
 import AboutHeader from "@/components/About/Header.vue";
 import AboutFooter from "@/components/About/Footer.vue";
 function enableCustomLayout() {
