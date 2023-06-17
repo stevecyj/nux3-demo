6a111d1f84baff68391c40fd5ee6c9dfdbc973d6
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-17 12:39:02 +0800

    feat: refactor about page layout and components
    - Change the layout of the about page from custom to default
    - Add a button to update the layout of the about page
    - Add a function to enable custom layout
    - Include a header and footer component in the about page
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,18 @@
 <script lang="ts" setup>
 definePageMeta({
-  layout: "custom",
+  layout: "default",
 });
 import AboutHeader from "@/components/About/Header.vue";
 import AboutFooter from "@/components/About/Footer.vue";
+function enableCustomLayout() {
+  setPageLayout("custom");
+}
 </script>
 
 <template>
   <AboutHeader />
   <div>Page: about</div>
+  <button @click="enableCustomLayout">Update layout</button>
   <AboutFooter />
 </template>
 
