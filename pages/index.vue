9f6ce2aec403100868995163d873a11abded4b5a
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-17 17:01:26 +0800

    feat: add API authentication middleware
    - Add a button to fetch data from an API in the index page
    - Add a new file `auth.ts` in the server middleware folder
    - Log the request URL in the `auth.ts` middleware file
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,16 @@
 const { $hello } = useNuxtApp();
 const store = useHomeStore();
 const selectedDate = ref(new Date());
+const fetchData = async () => {
+  const res = await fetch("/api/hello").then((res) => res.json());
+  console.log(res);
+};
 </script>
 
 <template>
   <div>
     <h1>index:{{ store.count }}</h1>
+    <button @click="fetchData">get api data</button>
     <ClientOnly>
       <h2>{{ $hello("IronMan") }}</h2>
     </ClientOnly>
