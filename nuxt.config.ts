e248789bc3cf4bffcdf9273cec937385f7494314
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-06-17 17:14:31 +0800

    style: update website configuration in nuxt.config.ts file
    - Change the viewport width to 500 pixels in `nuxt.config.ts`
    - Update the website title and meta information in `nuxt.config.ts`
    - Enable devtools in `nuxt.config.ts`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      viewport: "width=500, initial-scale=1",
+      title: "這是 JOJO 的好吃烹飪課程",
+      meta: [
+        {
+          name: "description",
+          content: "這是 JOJO 的好吃烹飪課程",
+        },
+        { property: "og:title", content: "這是 JOJO 的好吃烹飪課程" },
+        { property: "og:url", content: "http://localhost:3000/" },
+        {
+          property: "og:description",
+          content: "這是 JOJO 的好吃烹飪課程",
+        },
+      ],
+    },
+  },
+
   devtools: { enabled: true },
   components: {
     dirs: [
