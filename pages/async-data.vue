4a21d05096f606a16543d4fb6a5a8a1c57af6f1c
stevecyj <ycsteven.tsao@gmail.com>
2023-06-19 00:18:38 +0800

    feat: add refresh functionality to useAsyncData page
    - Add a refresh button to the useAsyncData page
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/async-data.vue
+++ pages/async-data.vue
@@ -2,12 +2,19 @@
 const { data } = await useAsyncData("nuxt3test", () => {
   return $fetch("https://api.nuxtjs.dev/mountains");
 });
+
+// 重新觸發更新資料
+const refreshGetData = () => {
+  refreshNuxtData("nuxt3test");
+  console.log("refreshGetData");
+};
 </script>
 
 <template>
   <div>
     <h1>useAsyncData</h1>
-    {{ data }}}
+    <button @click="refreshGetData">refresh</button>
+    {{ data }}
   </div>
 </template>
 
