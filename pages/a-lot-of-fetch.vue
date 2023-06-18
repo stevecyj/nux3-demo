e127fae1577e4b0d8ea95098679a0d316397c6e2
stevecyj <ycsteven.tsao@gmail.com>
2023-06-19 07:24:15 +0800

    chore: refactor API calls in "a-lot-of-fetch" page using axios
    - Add axios to the list of dependencies in `package.json`
    - Comment out unused code in `pages/a-lot-of-fetch.vue`
    - Replace `useFetch` with `axios` in `pages/a-lot-of-fetch.vue`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/a-lot-of-fetch.vue
+++ pages/a-lot-of-fetch.vue
@@ -1,19 +1,24 @@
 <script lang="ts" setup>
+import axios from "axios";
 // const { data: orgsData } = await useFetch("https://api.github.com/orgs/nuxt");
 // const { data: reposData } = await useFetch(
 //   "https://api.github.com/orgs/nuxt/repos"
 // );
-const [{ data: orgsData }, { data: reposData }] = await Promise.all([
-  useFetch(`https://api.github.com/orgs/nuxt`),
-  useFetch(`https://api.github.com/orgs/nuxt/repos`),
-]);
+
+// const [{ data: orgsData }, { data: reposData }] = await Promise.all([
+//   useFetch(`https://api.github.com/orgs/nuxt`),
+//   useFetch(`https://api.github.com/orgs/nuxt/repos`),
+// ]);
+const { data } = await useAsyncData("axiostest", async () => {
+  const { data } = await axios.get("https://api.github.com/orgs/nuxt");
+  return data;
+});
 </script>
 
 <template>
   <div>
     <h1>a lot of fetch</h1>
-    {{ orgsData }}
-    {{ reposData }}
+    {{ data }}
   </div>
 </template>
 
