8572ddfceac1e7de333586dca6e956709f9e268b
stevecyj <ycsteven.tsao@gmail.com>
2023-06-23 11:08:21 +0800

    feat: add custom 404 page for website
    - Add new file `pages/[...slug].vue` with 404 page code
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

---
+++ pages/[...slug].vue
@@ -0,0 +1,14 @@
+<script setup>
+const route = useRoute();
+const event = useRequestEvent();
+setResponseStatus(event, 404);
+</script>
+
+<template>
+  <div>
+    <h1>404 not found</h1>
+    <p>{{ route.params.slug }}</p>
+  </div>
+</template>
+
+<style></style>
