5119432cc8d4acad187b80241b5ce7fac0b01f0d
stevecyj <ycsteven.tsao@gmail.com>
2023-06-23 10:49:12 +0800

    feat: add loading spinner to page transitions
    - Add loading spinner functionality to page transitions
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const nuxtApp = useNuxtApp();
+const isLoading = ref(false);
+
+nuxtApp.hook("page:start", () => {
+  isLoading.value = true;
+});
+nuxtApp.hook("page:finish", () => {
+  isLoading.value = false;
+});
+</script>
 
 <template>
   <div>
-    <NuxtLoadingIndicator color="#f00" :height="5" :throttle="0" />
+    <!-- <NuxtLoadingIndicator color="#f00" :height="5" :throttle="0" /> -->
+    <div v-show="isLoading">Loading</div>
     <header>
       <NuxtLink to="/">Home</NuxtLink>
       <NuxtLink to="/courses">Courses</NuxtLink>
